2c5230d74449e98ab64eaccc169f1a4fc650110d
Pedro Conde <pconde007@gmail.com>
2022-11-28 10:54:04 +0000

    Sección 6 lista

--- routes/router.ts
+++ routes/router.ts
@@ -1,6 +1,7 @@
 
 import { Router, Request, Response } from 'express';
 import Server from '../classes/server';
+import { usuariosConectados } from '../sockets/socket';
 
 const router = Router();
 
@@ -57,6 +58,34 @@ router.post('/mensajes/:id', ( req: Request, res:Response ) => {
 
 });
 
+// Servicio para obtener todos los IDs de los usuarios
+router.get('/usuarios',(req:Request,res:Response)=>{
+    const server=Server.instance;
+    server.io.allSockets().then((clientes)=>{
+        res.json({
+            ok:true,
+           // clientes
+            clientes: Array.from(clientes)
+        });
+    }).catch((err)=>{
+        res.json({
+            ok:false,
+            err
+        })
+    });
+});
+
+// Obtener usuarios y sus nombres
+router.get('/usuarios/detalle',(req:Request,res:Response)=>{
+
+    res.json({
+        ok: true,
+        clientes: usuariosConectados.getLista()
+    });
+
+    
+
+});
 
 
 export default router;
